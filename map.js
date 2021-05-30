dd76f8860b5469596cd4bc0a17e833d5321a7900
AndreaEPFL <56548559+AndreaEPFL@users.noreply.github.com>
2021-05-30 14:44:43 +0200

    Latest updates

--- map.js
+++ map.js
@@ -7,10 +7,10 @@ class MapPlot {
         opacity: 1,
         color: 'white',
         dashArray: '1',
-        fillOpacity: 0.9
+        fillOpacity: 0.9,
         };
 
-    // style for when mouse hover over country
+    // border style for when mouse hover over country
     hoverStyle = {
         weight: 2,
         color: '#666',
@@ -34,7 +34,21 @@ class MapPlot {
             zoomControl: false, dragging:false, doubleClickZoom: false, zoomSnap:0.1}).setView([51, 9]).setZoom(4.3);
 
         var selectedCountries = [];
-        var selectedColors = ['red', 'green', 'blue']
+        const color_func = {AT: "#FF0000",
+                        BE: "#FF8000",
+                        CH: "#FFFF00",
+                        DE: "#33ff00",
+                        DK: "#2243B6",
+                        ES: "#5DADEC",
+                        FR: "#9C51B6",
+                        GB: "#A83731",
+                        IE: "#AF6E4D",
+                        IT: "#E936A7",
+                        LU: "#00ffe1",
+                        NL: "#ff0099",
+                        NO: "#1d8000",
+                        PT: "#694600",
+                        SE: "#652c77"};
 
         // Draw full map
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -46,7 +60,6 @@ class MapPlot {
             zoomOffset: -1
         }).addTo(this.map);
 
-        // Unnecessary ?
         this.countries = ["AT", "BE", "CH", "DE", "DK", "ES", "FR", "GB", "IE", "LT", "LU", "NL", "NO", "PT", "SE"];
 
         this.info = L.control();
@@ -57,19 +70,24 @@ class MapPlot {
         	return this._div;
         };
 
-        const minimum = Math.min(...Object.values(energy_consumption[0]));
-        const maximum = Math.max(...Object.values(energy_consumption[0]));
+
+        // Copy energy_consumption to remove dates and get true maximum value
+        var energy_copy = JSON.parse(JSON.stringify(energy_consumption));
+        delete energy_copy[0]['Date'];
+
+        // Get the minimum and maximum values
+        const minimum = Math.min(...Object.values(energy_copy[0]));
+        const maximum = Math.max(...Object.values(energy_copy[0]));
 
         this.info.update = function (feat) {
         	this._div.innerHTML = '<h4>Energy consumption</h4>' +  (feat ?
-        		'<b>' + feat.properties.NAME + '</b><br />' + energy_consumption[0][feat.properties.ISO2] + ' MWh '
+        		'<b>' + feat.properties.NAME + '</b><br />' + MapAttributes.energy_consumption[0][feat.properties.ISO2] + ' MWh '
         		: 'Hover over a state');
         };
 
         this.info.addTo(this.map);
 
-
-        // Function to associate energy consumption value with fill in
+        // Function to associate energy consumption value with fill in color
         function style_country(features) {
             var style = MapAttributes.defaultStyle;
             style.fillColor = MapAttributes.getColorScale(MapAttributes.energy_consumption[0][features.properties.ISO2],
@@ -101,7 +119,6 @@ class MapPlot {
             const layer = e.target;
 
             if (selectedCountries.includes(layer._leaflet_id)) {
-                selectedColors.push(layer.options.color);
                 MapAttributes.geojson.resetStyle(layer)
                 const index = selectedCountries.indexOf(layer._leaflet_id);
                 if (index > -1) {
@@ -110,7 +127,7 @@ class MapPlot {
                 main.RemoveCountry(layer.feature.properties.ISO2)
             }
             else {
-                var color = selectedColors.pop();
+                var color = color_func[layer.feature.properties.ISO2];
                 var style = MapAttributes.selectedStyle;
                 style.color = color;
                 layer.setStyle(style);
@@ -145,13 +162,18 @@ class MapPlot {
             onEachFeature: onEachFeature
         }).addTo(this.map);
 
-        var legend = L.control({position: 'bottomright'});
+        this.legend = L.control({position: 'bottomright'});
 
-        legend.onAdd = function (map) {
+        this.legend.onAdd = function (map) {
+            const N = 10; // Number of squares
+            grades = [];
+            for (var j = 0; j <= N; j++) {
+                // Logarithmic scale: grades.push(Math.round(minimum*((maximum/minimum)**(j/N))/100)*100);
+                grades.push(Math.round((minimum+j*(maximum-minimum)/N)/100)*100)
+            };
 
         var div = L.DomUtil.create('div', 'info legend'),
-          grades = [0, 800, 900, 1000, 2000, 3000, 4000, 5000,
-              6000, 7000, 8000, 9000, 10000, 20000, 30000],
+          grades,
           labels = [],
           from, to;
 
@@ -160,7 +182,7 @@ class MapPlot {
             from = grades[i];
             to = grades[i + 1];
             labels.push(
-        				'<i style="background:' + MapAttributes.getColor(from + 1) + '"></i> ' +
+        				'<i style="background:' + MapAttributes.getColorScale(from + 1, minimum, maximum) + '"></i> ' +
         				from + (to ? '&ndash;' + to : '+'));
         }
 
@@ -168,59 +190,19 @@ class MapPlot {
             return div;
         };
 
-        legend.addTo(this.map);
-        // Test some shit -> check console
-        //console.log(energy_consumption[0])
+        this.legend.addTo(this.map);
     }
 
     getColorScale(d, min, max) {
         // Minimum value, (min+max)/2, maximum value
         let linearScale = d3.scaleLinear()
                         .domain([min, (min+max)/2, max])
-                        .range(['#4c080f', '#f09c31', '#ffed7e']);
+                        .range(['#ffed7e', '#f09c31', '#4c080f']);
         return linearScale(d)
     }
-    getColor(d) {
-          return d > 30000 ? '#340000' :
-              d > 25000 ? '#400408' :
-              d > 20000 ? '#4c080f' :
-              d > 15000 ? '#590f13' :
-              d > 10000 ? '#661516' :
-              d > 9500  ? '#731c18' :
-              d > 9000  ? '#80231a' :
-              d > 8500  ? '#8d2b1c' :
-              d > 8000  ? '#9a331d' :
-              d > 7500  ? '#a73b1e' :
-              d > 7000  ? '#b4441f' :
-              d > 6500  ? '#c04d1e' :
-              d > 6000  ? '#cd561d' :
-              d > 5500  ? '#d9601c' :
-              d > 5000  ? '#e56a19' :
-              d > 4500  ? '#e7751c' :
-              d > 4000  ? '#ea7f20' :
-              d > 3500  ? '#ec8925' :
-              d > 3000  ? '#ee922b' :
-              d > 2500  ? '#f09c31' :
-              d > 2000  ? '#f2a538' :
-              d > 1500  ? '#f4af3f' :
-              d > 1000  ? '#f5b847' :
-              d > 950   ? '#f7c14f' :
-              d > 900   ? '#f9ca58' :
-              d > 850   ? '#fad361' :
-              d > 800   ? '#fcdc6a' :
-              d > 750   ? '#fde474' :
-              d > 700   ? '#fee778' :
-              d > 650   ? '#ffed7e' :
-                  '#f9e1a2';
-                      }
 
-    updateMap(start, end) {
+    updateMap(data) {
             const MapAttributes = this;
-
-        // Get entries from start to end dates
-            const data = MapAttributes.energy_consumption.filter(row => { //filter the time
-                return row.Date.getTime() >= start.getTime() && row.Date.getTime() < end.getTime()
-            });
 
           // Get months for each entry
             const data_month = data.map(x => ({...x, Month: new Date(x.Date).getMonth()}));
@@ -235,20 +217,57 @@ class MapPlot {
             }, {})
 
             const data_average = Object.keys(data_sum).reduce((acc, key) => {acc[key] = data_sum[key]/count; return acc; }, {})
-            //console.log(data_average)
 
             // Get minimum and maximum values for color scale
-            const minimum = Math.min(...data_average);
-            const maximum = Math.max(...data_average);
-
-            // For now, plots the first entry in [start, end]
-            const data_test = MapAttributes.energy_consumption.filter(row => { //filter the time
-                return row.Date.getTime() >= start.getTime() && row.Date.getTime() <= start.getTime()
-            });
+            var energy_copy = JSON.parse(JSON.stringify(data_average));
+            for(var z = 0; z <energy_copy.length; z++) {
+                delete energy_copy[z]['Date'];
+            }
+            const minimum = Math.min(...Object.values(energy_copy));
+            const maximum = Math.max(...Object.values(energy_copy));
+            
 
             function country_style(feat) {
                     return { fillColor: MapAttributes.getColorScale(data_average[feat.properties.ISO2], minimum, maximum)};
             }
             this.geojson.setStyle(country_style);
+            MapAttributes.updateLegend(energy_copy);
+        }
+
+        updateLegend(data) {
+        const MapAttributes = this;
+        const minimum = Math.min(...Object.values(data));
+        const maximum = Math.max(...Object.values(data));
+
+        MapAttributes.map.removeControl(MapAttributes.legend);
+
+        MapAttributes.legend = L.control({position: 'bottomright'});
+
+        MapAttributes.legend.onAdd = function (map) {
+            const N = 10; // Number of squares
+            grades = [];
+            for (var j = 0; j <= N; j++) {
+                // Logarithmic scale: grades.push(Math.round(minimum*((maximum/minimum)**(j/N))/100)*100);
+                grades.push(Math.round((minimum+j*(maximum-minimum)/N)/100)*100)
+            }
+            var div = L.DomUtil.create('div', 'info legend'),
+            grades,
+            labels = [],
+            from, to;
+
+            // loop through our density intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < grades.length; i++) {
+                from = grades[i];
+                to = grades[i + 1];
+                labels.push(
+        				'<i style="background:' + MapAttributes.getColorScale(from + 1, minimum, maximum) + '"></i> ' +
+        				from + (to ? '&ndash;' + to : '+'));
+            }
+
+            div.innerHTML = labels.join('</br>');
+            return div;
+        };
+
+        MapAttributes.legend.addTo(this.map);
         }
 }
